ec3c263b8675e9cf4308f06e3f321e3b1bd13063
cnkoster <noor.koster@cern.ch>
2023-11-15 10:00:00 +0100

    print pt bins

--- Analysis/CalculateFlow.cxx
+++ Analysis/CalculateFlow.cxx
@@ -1576,7 +1576,7 @@ void CalculateFlow::CalculateFlowQC()
 
         if(qpM0>0 && QM0>0) {
           dQC2 = (qpRe0*QRe+qpIm0*QIm-qpM)/dQM2;
-          std::cout<<"pt bin: "FillPtBin<< " filled with : " << dQC2 << std::endl;
+          std::cout<<"pt bin: " << FillPtBin<< " filled with : " << dQC2 << std::endl;
           fFlowQCCorPro[fCenBin][hr][1][charge]->Fill(FillPtBin,dQC2,WdQM2*fCenWeightEbE);
           dQ2f = kTRUE;
         }
