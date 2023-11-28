1b2c85b35204a80e44b73b44647b7ed59b33305f
cnkoster <noor.koster@cern.ch>
2023-11-28 14:17:31 +0100

    SPM and 5 eta bins

--- Analysis/runFlow.C
+++ Analysis/runFlow.C
@@ -95,12 +95,12 @@ void runFlow(Bool_t etaFlag = kTRUE, TString centrality="", Double_t gCentrality
 
     }
     fQC->Terminate(nevent);
-
+  std::cout<<"here we go"<<std::endl; 
     
     // Save list holding histogram with weights:
     TFile *fResultsFile = new TFile(Form(//"AnalysisResults_Group%i-%i_%sDiff_All.root",iGroupMin, iGroupMax, diff.Data()),"RECREATE");
                                          //"AnalysisResults_Group%i-%i_%sDiff_Full_Cent%s.root",iGroupMin, iGroupMax, diff.Data(), centrality.Data()),"RECREATE");
-                                         "AnalysisResults_Group%i-%i_etaDiff_SPMEPM_3050.root",iGroupMin, iGroupMax),"RECREATE");
+                                         "AnalysisResults_Group0-6000_etaDiff_SPMEPM_3050.root"),"RECREATE");
     fResultsFile->WriteObject(fQC->GetQAList(),"QAList","SingleKey");
     fResultsFile->WriteObject(fQC->GetSpectraList(),"SpectraList","SingleKey");
     //fResultsFile->WriteObject(fQC->GetFlowQCList(),"FLowQCList","SingleKey");
